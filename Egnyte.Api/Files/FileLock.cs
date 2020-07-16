3a06b66b31ac3154d4f3951adfdab11949252d81
Llewellyn Roos <lroos@openboxsoftware.com>
2020-07-17 01:58:11 +0200

    Add missing file lock class

---
+++ Egnyte.Api/Files/FileLock.cs
@@ -0,0 +1,13 @@
+﻿namespace Egnyte.Api.Files
+{
+    using Newtonsoft.Json;
+
+    public class FileLock
+    {
+        [JsonProperty(PropertyName = "timeout")]
+        public long Timeout { get; set; }
+
+        [JsonProperty(PropertyName = "lock_token")]
+        public string LockToken { get; set; }
+    }
+}
