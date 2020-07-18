11a032c96fabb82d13e1aa3969851ed2912e2506
Llewellyn Roos <lroos@openboxsoftware.com>
2020-07-18 23:33:19 +0200

    .NET Standard project upgrade

--- Egnyte.Api.Tests/Files/DownloadFileTests.cs
+++ Egnyte.Api.Tests/Files/DownloadFileTests.cs
@@ -2,21 +2,20 @@
 {
     using System;
     using System.Globalization;
+    using System.Linq;
     using System.Net;
     using System.Net.Http;
     using System.Threading.Tasks;
-
     using Egnyte.Api.Files;
-
     using NUnit.Framework;
-    using System.Linq;
+
     [TestFixture]
     public class DownloadFileTests
     {
-        const string Checksum = "6cb2785692b05c5eff397109457031bde7ab236982364cc7b51e319c67c463d7721c82c024ef3f74b9dff d388be6dc8120edc214e7d0eadaaf2c5e0eb44845a3";
-        const string ETag = "9c4c2443-5dbc-4afa-8d04-5620a778093c";
-        const string ContentType = "text/plain; charset=UTF-8";
-        const int ContentLength = 126;
+        private const string Checksum = "6cb2785692b05c5eff397109457031bde7ab236982364cc7b51e319c67c463d7721c82c024ef3f74b9dff d388be6dc8120edc214e7d0eadaaf2c5e0eb44845a3";
+        private const string ETag = "9c4c2443-5dbc-4afa-8d04-5620a778093c";
+        private const string ContentType = "text/plain; charset=UTF-8";
+        private const int ContentLength = 126;
 
         [Test]
         public async Task DownloadFile_ReturnsCorrectFile()
@@ -70,7 +69,6 @@
             Assert.AreEqual("https://acme.egnyte.com/pubapi/v1/fs-content/myFile", requestMessage.RequestUri.ToString());
         }
 
-
         [Test]
         public async Task DownloadFile_WithWrongRange_ThrowsArgumentOutOfRangeException()
         {
@@ -83,7 +81,7 @@
             var egnyteClient = new EgnyteClient("token", "acme", httpClient);
 
             var exception = await AssertExtensions.ThrowsAsync<ArgumentOutOfRangeException>(
-                () => egnyteClient.Files.DownloadFile("myFile", new Range(1000, 0)));
+                () => egnyteClient.Files.DownloadFile("myFile", new Api.Files.Range(1000, 0)));
 
             Assert.IsTrue(exception.Message.Contains("'From' parameter must be less or equal to 'to'"));
             Assert.IsNull(exception.InnerException);
@@ -99,7 +97,7 @@
                 (request, cancellationToken) => Task.FromResult(GetResponseMessage());
 
             var egnyteClient = new EgnyteClient("token", "acme", httpClient);
-            var result = await egnyteClient.Files.DownloadFile("myFile", new Range(0, 100))
+            var result = await egnyteClient.Files.DownloadFile("myFile", new Api.Files.Range(0, 100))
                 .ConfigureAwait(false);
 
             var requestMessage = httpHandlerMock.GetHttpRequestMessage();
@@ -118,7 +116,7 @@
             Assert.AreEqual(ContentLength, result.ContentLength);
             Assert.AreEqual(999, result.FullFileLength);
         }
-        
+
         [Test]
         public async Task DownloadFile_WithEntryId_ReturnsCorrectFile()
         {
@@ -146,7 +144,7 @@
             Assert.AreEqual(999, result.FullFileLength);
         }
 
-        HttpResponseMessage GetResponseMessage()
+        private HttpResponseMessage GetResponseMessage()
         {
             var response = GetResponseWithoutContentRangeMessage();
             response.Content.Headers.Add("Content-Range", "bytes 0-100/999");
@@ -154,7 +152,7 @@
             return response;
         }
 
-        HttpResponseMessage GetResponseWithoutContentRangeMessage()
+        private HttpResponseMessage GetResponseWithoutContentRangeMessage()
         {
             var responseMessage = new HttpResponseMessage
             {
