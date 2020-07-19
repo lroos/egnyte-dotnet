56814336b08ba90ca76d1821afd23a0868de64cf
Llewellyn Roos <lroos@openboxsoftware.com>
2020-07-19 17:52:30 +0200

    Merge file audit report API support from: https://github.com/mikuam/egnyte-dotnetcore
    (cherry picked from commit 0b4633b471b1d42d6e0c49e5ae37c149320966e4)

---
+++ Egnyte.Api/Audit/CreateFileAuditReportResponse.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace Egnyte.Api.Audit
+{
+    internal class CreateFileAuditReportResponse
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+    }
+}
---
+++ Egnyte.Api/Audit/IAuditReportResponse.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Egnyte.Api.Audit
+{
+    public interface IAuditReportResponse
+    {
+        string Status { get; }
+
+        int TotalCount { get; }
+
+        int Offset { get; }
+
+        int Count { get; }
+
+        List<IAuditResponseItem> Events { get; }
+    }
+}
